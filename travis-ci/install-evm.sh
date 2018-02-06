97e545f80d5656635a908a53ec2ae41fcb340908
Neil Okamoto <neil.okamoto@gmail.com>
2018-02-05 22:35:07 -0800

    [Fix #1] expand Travis CI build matrix

---
+++ travis-ci/install-evm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+# Install evm for Travis CI
+# or if already installed, then check for updates
+
+WORKDIR=${HOME}/local
+EVMDIR=$WORKDIR/evm
+SCRIPTDIR=`dirname $(readlink -f $0)`
+
+. $SCRIPTDIR/retry.sh
+
+if [ -d $EVMDIR ]
+then
+    cd $EVMDIR
+    git pull origin master
+else
+    git clone https://github.com/rejeep/evm.git $EVMDIR
+    evm config path /tmp
+fi
